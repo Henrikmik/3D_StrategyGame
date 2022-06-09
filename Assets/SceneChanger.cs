efa3583d667c2178f31d5e55aa74e1117da0bd04
Kauz Andrej <kauz-andrej@web.de>
2022-06-09 18:45:41 +0200

    Intro Scene functionality + Fade between Scenes added

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private int sceneToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1){
+            if (Input.GetKeyDown(KeyCode.P))
+                    {
+                        FadeToNextScene();
+                    }
+        }
+        
+    }
+
+    public void FadeToNextScene()
+    {
+        FadeToScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void FadeToScene (int sceneIndex)
+    {
+        sceneToLoad = sceneIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+}
